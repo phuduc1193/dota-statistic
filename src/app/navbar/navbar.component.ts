5c6f42b552953755b8fc0e8fb4e2115401ed08d7
Duc Truong <phuduc1193@gmail.com>
2017-02-16 06:26:28 +0000

    Correct collapse navbar function for mobile

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,12 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+  host: { '(document:click)': 'onClick($event)' }
 })
 export class NavbarComponent implements OnInit {
   public isCollapsed: boolean
   constructor() { this.isCollapsed = true }
 
   ngOnInit() {
+  }
+  
+  onClick($event: any){
+    if (($event.target.classList.contains('navbar-toggle')) || ($event.target.classList.contains('icon-bar')))
+      this.isCollapsed = !this.isCollapsed
+    else this.isCollapsed = true
   }
 }
