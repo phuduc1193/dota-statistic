013ba3c883ddcc07c04e167d9cf61fb500814205
Duc Truong <phuduc1193@gmail.com>
2017-02-16 00:22:24 +0000

    Add routingAnimations

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,17 +2,19 @@ import {
   Component, OnInit,
   trigger, state, animate, transition, style 
 } from '@angular/core';
+import { routerTransition } from '../router.animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  host: {'[@routerTransition]': ''},
   animations: [
     trigger('visibilityChanged', [
       state('true' , style({ opacity: 1, transform: 'scale(1.0)' })),
       state('false', style({ opacity: 0, transform: 'scale(0.0)'  })),
       transition('1 => 0', animate('500ms')),
       transition('0 => 1', animate('900ms'))
-    ])
+    ]), routerTransition()
   ]
 })
 export class HomeComponent implements OnInit {
